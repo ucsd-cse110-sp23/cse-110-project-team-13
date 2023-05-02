dc3f8d901ade1b0400ab54dc8a4574e02a340769
Georgexli3 <103277409+Georgexli3@users.noreply.github.com>
2023-05-02 15:54:05 -0700

    Changed API Key to correct one

--- SayIt ChatGPT/ChatGPT Response/src/ChatGPT.java
+++ SayIt ChatGPT/ChatGPT Response/src/ChatGPT.java
@@ -8,7 +8,7 @@ import org.json.JSONObject;
 
 public class ChatGPT {
     private static final String API_ENDPOINT = "https://api.openai.com/v1/completions";
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "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";
     private static final String MODEL = "text-davinci-003";
 
     public static void main(String[] args) throws IOException, InterruptedException{
