e50cf9b0a3435b6d500b95795590e1749e493652
Georgexli3 <103277409+Georgexli3@users.noreply.github.com>
2023-05-31 01:15:22 -0700

    Updated to store username and password

--- src/main/java/Create.java
+++ src/main/java/Create.java
@@ -17,24 +17,16 @@ public class Create {
 
     public static void main(String[] args) {
         String uri = "mongodb://gll002:5XzFoHTsaDuKyO4o@ac-jp7sh2s-shard-00-00.ws94cg8.mongodb.net:27017,ac-jp7sh2s-shard-00-01.ws94cg8.mongodb.net:27017,ac-jp7sh2s-shard-00-02.ws94cg8.mongodb.net:27017/?ssl=true&replicaSet=atlas-12j7mx-shard-0&authSource=admin&retryWrites=true&w=majority";
+        
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-
-
             MongoDatabase sampleTrainingDB = mongoClient.getDatabase("sample_training");
-            MongoCollection<Document> gradesCollection = sampleTrainingDB.getCollection("grades");
+            MongoCollection<Document> usersCollection = sampleTrainingDB.getCollection("users");
 
+            Document user = new Document("_id", new ObjectId());
+            user.append("username", "testUser")
+                .append("password", "testPassword");
 
-            Random rand = new Random();
-            Document student = new Document("_id", new ObjectId());
-            student.append("student_id", 10000d)
-                   .append("class_id", 1d)
-                   .append("scores", asList(new Document("type", "exam").append("score", rand.nextDouble() * 100),
-                                            new Document("type", "quiz").append("score", rand.nextDouble() * 100),
-                                            new Document("type", "homework").append("score", rand.nextDouble() * 100),
-                                            new Document("type", "homework").append("score", rand.nextDouble() * 100)));
-
-
-            gradesCollection.insertOne(student);
+            usersCollection.insertOne(user);
         }
     }
 }
--- src/main/java/CreateMultiple.java
+++ src/main/java/CreateMultiple.java
@@ -17,51 +17,36 @@ import static java.util.Arrays.asList;
 
 public class CreateMultiple {
 
-
-    private static final Random rand = new Random();
-
-
     public static void main(String[] args) {
         String uri = "mongodb://gll002:5XzFoHTsaDuKyO4o@ac-jp7sh2s-shard-00-00.ws94cg8.mongodb.net:27017,ac-jp7sh2s-shard-00-01.ws94cg8.mongodb.net:27017,ac-jp7sh2s-shard-00-02.ws94cg8.mongodb.net:27017/?ssl=true&replicaSet=atlas-12j7mx-shard-0&authSource=admin&retryWrites=true&w=majority";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-
-
             MongoDatabase sampleTrainingDB = mongoClient.getDatabase("sample_training");
-            MongoCollection<Document> gradesCollection = sampleTrainingDB.getCollection("grades");
+            MongoCollection<Document> usersCollection = sampleTrainingDB.getCollection("users");
 
-
-            insertOneDocument(gradesCollection);
-            insertManyDocuments(gradesCollection);
+            insertOneUser(usersCollection);
+            insertManyUsers(usersCollection);
         }
     }
 
-
-    private static void insertOneDocument(MongoCollection<Document> gradesCollection) {
-        gradesCollection.insertOne(generateNewGrade(10000d, 1d));
-        System.out.println("One grade inserted for studentId 10000.");
+    private static void insertOneUser(MongoCollection<Document> usersCollection) {
+        usersCollection.insertOne(generateNewUser("testUser1", "testPassword1"));
+        System.out.println("One user inserted with username testUser1.");
     }
 
-
-    private static void insertManyDocuments(MongoCollection<Document> gradesCollection) {
-        List<Document> grades = new ArrayList<>();
-        for (double classId = 1d; classId <= 10d; classId++) {
-            grades.add(generateNewGrade(10001d, classId));
+    private static void insertManyUsers(MongoCollection<Document> usersCollection) {
+        List<Document> users = new ArrayList<>();
+        for (int i = 2; i <= 11; i++) {
+            users.add(generateNewUser("testUser" + i, "testPassword" + i));
         }
 
-
-        gradesCollection.insertMany(grades, new InsertManyOptions().ordered(false));
-        System.out.println("Ten grades inserted for studentId 10001.");
+        usersCollection.insertMany(users, new InsertManyOptions().ordered(false));
+        System.out.println("Ten users inserted.");
     }
 
-
-    private static Document generateNewGrade(double studentId, double classId) {
-        List<Document> scores = asList(new Document("type", "exam").append("score", rand.nextDouble() * 100),
-                                       new Document("type", "quiz").append("score", rand.nextDouble() * 100),
-                                       new Document("type", "homework").append("score", rand.nextDouble() * 100),
-                                       new Document("type", "homework").append("score", rand.nextDouble() * 100));
-        return new Document("_id", new ObjectId()).append("student_id", studentId)
-                                                  .append("class_id", classId)
-                                                  .append("scores", scores);
+    private static Document generateNewUser(String username, String password) {
+        return new Document("_id", new ObjectId())
+                .append("username", username)
+                .append("password", password);
     }
 }
 
